e138d16a16735951a8261e945444bb2e6dc0592d
Anusha Konatala <anu.konatala@gmail.com>
2018-06-18 23:00:16 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,7 @@
 class Post
 attr_accessor :title, :author
-def initialize(title, genre)
-  @name = name
-  @genre = genre
+def initialize(title, author)
+  @title = title
+  @author = author
 end
 end
